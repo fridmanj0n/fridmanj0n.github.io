6fceabb7e397f8db974e2a2cd7b6cacc1c583b61
eevee12312 <75412382+eevee12312@users.noreply.github.com>
2023-11-22 11:07:14 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('createCube', (cubeData) => {
+    io.emit('cubeCreated', cubeData);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
